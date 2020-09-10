6b491c058ffb6f53afe5fc49a4964997551d60ab
nilton santos <jose.santos@sou.fafram.com.br>
2020-09-09 22:43:04 -0300

    segunda alteracao

--- Teste/teste.js
+++
@@ -1,3 +0,0 @@
-let a = 20
-
-console.log(a)
--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express') 
 const app = express()
+const bodyParse = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+const uri = mongodb+srv://<username>:<password>@cluster0.vkbem.mongodb.net/<dbname>?retryWrites=true&w=majority
 
 app.listen(3000, function(){
     console.log('rodando...')
@@ -10,3 +13,4 @@ app.get('/',(req,res) => {
 })
 
 app.set('view engine', 'ejs')
+
