2668ffba4484d2b12dbad4124963f044bcb38e16
nilton santos <jose.santos@sou.fafram.com.br>
2020-09-11 22:54:56 -0300

    conexao banco

--- server.js
+++ server.js
@@ -1,16 +1,30 @@
-const express = require('express') 
+const express = require('express')
 const app = express()
-const bodyParse = require('body-parser')
+const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient
-const uri = mongodb+srv://<username>:<password>@cluster0.vkbem.mongodb.net/<dbname>?retryWrites=true&w=majority
+const uri = "mongodb+srv://fafram:@fafram*2020@cluster0.vkbem.mongodb.net/<dbname>?retryWrites=true&w=majority"
 
-app.listen(3000, function(){
-    console.log('rodando...')
-})
+app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get('/',(req,res) => {
+app.get('/', (req, res) => {
     res.render('index.ejs')
 })
-
 app.set('view engine', 'ejs')
 
+app.post('/show', (req, res) => {
+    db.collection('alunos').save(req.body, (err, result) => {
+        if(err) return console.log(err)
+
+        console.log('Salvo no banco de dados')
+        res.redirect('/')
+    }) 
+})
+
+MongoClient.connect(uri, (err, client) => {
+    if (err) return console.log(err)
+    db = client.db('fafram')
+
+    app.listen(3000, () => {
+        console.log('Servidor rodando na porta 3000')
+    })
+})
